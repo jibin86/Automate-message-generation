4b9320423b22b028e6f233c9de86499a4b4569e3
Jibin-Song <sjbpsh1@sookmyung.ac.kr>
2023-04-04 23:57:40 +0900

    feat: generate text

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
-from datetime import datetime
+from flask import Flask, render_template, request, send_file, url_for, redirect
+from datetime import datetime, timedelta
+import read_excel
+from io import BytesIO
+from zipfile import ZipFile
+import os
+
 
 app = Flask(__name__)
 
@@ -9,16 +14,57 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    date = request.form.get("date")
-    next_hw = request.form.get("next_hw")
-    grade = request.form.get("grade")
-    excel_test = request.files.get("excel_test")
-    excel_hw = request.files.get("excel_hw")
-    excel_daily = request.files.get("excel_daily")
-    
-    
+    try:
+        date = request.form.get("date")
+        next_hw_str = request.form.get("next_hw")
+        grade = request.form.get("grade")
+        excel_test = request.files.get("excel_test")
+        excel_hw = request.files.get("excel_hw")
+        excel_daily = request.files.get("excel_daily")
+        
+        saturday = datetime.strptime(date, '%Y-%m-%d')   
+        sunday = saturday + timedelta(days=1)
+        date_list = [(saturday.month, saturday.day), (sunday.month, sunday.day)]
+        print('date_list', date_list)
+        
+        read_e = read_excel.Create_Message(date_list)
+        
+        df_hw = read_e.find_hw(excel_hw, date_list)
+        df_daily = read_e.find_daily(excel_daily, date_list)
+        df_test, mean_score = read_e.find_test(excel_test, date_list)
+        df_students = read_e.merge_df(df_hw, df_daily, df_test, col_name='이름')
+        
+        df_students['text'] = df_students.apply(read_e.make_text, args=(date_list, mean_score, next_hw_str), axis=1)
+        # folder_name = f'고{grade}_{date_list[0][0]}월_{date_list[0][1]}일_{date_list[1][0]}월_{date_list[1][1]}일_문자'
+        folder_name = 'folder_text'
+        read_e.save_text_files(folder_name, df_students)
+    except:
+        return render_template('fail.html')
+    return redirect(url_for('download_files', folder_name=folder_name))
 
-    return "Form submitted successfully"
+
+@app.route('/download_zip/<folder_name>')
+def download_files(folder_name):
+    # 폴더에서 텍스트 파일들을 불러옴
+    files = [f for f in os.listdir("folder_text") if f.endswith(".txt")]
+    # 텍스트 파일들과 함께 HTML 페이지 렌더링
+    return render_template("text_files.html", files=files)
+
+@app.route("/download/<filename>")
+def download_file(filename):
+    # 다운로드 링크를 눌렀을 때 해당 파일 다운로드
+    return send_file(os.path.join("folder_text", filename), as_attachment=True)
+
+@app.route('/download_all')
+def download_all_files():
+    folder_path = 'folder_text'
+    zip_filename = 'all_files.zip'
+    # create a zip file with all the files in the folder
+    with ZipFile(zip_filename, 'w') as zip_file:
+        for file in os.listdir(folder_path):
+            zip_file.write(os.path.join(folder_path, file), file)
+    # send the zip file to the client
+    return send_file(zip_filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ read_excel.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+import os
+
+class Create_Message:
+    def __init__(self, date):
+        self.date = date
+        
+    # 데이터프레임 앞뒤 공백 제거
+    def clean_space_df(self, df):
+        df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        return df
+
+    # 특정 날짜, 데이터프레임 반환
+    def get_name_date_df(self, path, date):
+        df = pd.read_excel(path, header=[1, 2])
+        df = self.clean_space_df(df)
+        hw_col = [col for col in df.columns if (col[0].startswith(str(date[0][0])+'월') and  str(date[0][1]) in col[0]) or (col[0].startswith(str(date[1][0])+'월') and  str(date[1][1]) in col[0])]
+        # 이름 공백 버리기
+        df.iloc[:,0] = df.iloc[:,0].fillna('0')
+        # 이름 아닌 부분 자르기
+        df_name_date = df[df.iloc[:,0].apply(lambda x: 2<=len(x)<=4)][[df.columns[0]]+hw_col]
+        return df_name_date
+
+    def find_hw(self, path, date):
+        df_hw = self.get_name_date_df(path, date)
+        df_hw.columns = df_hw.columns.droplevel()
+        df_hw = df_hw.drop(columns=df_hw.columns[2:])
+        # A, B, C, D 외에 존재하는 것들 다 D로 변경
+        hw_score_list = ['A', 'B', 'C', 'D']
+        df_hw.loc[~df_hw['숙제 제출 여부'].isin(hw_score_list),'숙제 제출 여부'] = 'D'
+        return df_hw
+
+    def find_daily(self, path,date):
+        df_daily = self.get_name_date_df(path, date)
+        df_daily.columns = df_daily.columns.droplevel()
+        df_daily['daily_done'] = df_daily.iloc[:,1:].apply(self.get_daily_done, axis=1)
+        df_daily['daily_not_done'] = df_daily.iloc[:,1:].apply(self.get_daily_not_done, axis=1)
+        return df_daily
+
+    def find_test(self, path, date):
+        df_test = self.get_name_date_df(path, date)
+        df_test.columns = df_test.columns.droplevel()
+        
+        # 평균 점수
+        mean_score = df_test['점수'].mean()
+        mean_score = int(np.round(mean_score))
+        
+        # 결측치 점수 => 미응시로 채우기
+        df_test = df_test.fillna('미응시')
+        return df_test, mean_score
+
+    def get_daily_done(self, df_daily_row):
+        done_list = df_daily_row[df_daily_row.notnull()].index
+        return list(done_list)
+
+    def get_daily_not_done(self, df_daily_row):
+        done_list = df_daily_row[df_daily_row.isnull()].index
+        return list(done_list)
+
+    def merge_df(self, df_daily, df_hw, df_test, col_name='이름'):
+        df_merge = pd.merge(df_daily, df_hw, on=col_name)
+        df_merge = pd.merge(df_merge, df_test, on=col_name)
+        return df_merge
+
+    # 일일학습지 문자
+    def mk_daily_str(self, student):
+        
+        # 일일학습지
+        if len(student['daily_done']) == 0:
+            daily_str = f"일일학습지는 {' / '.join(student['daily_not_done'])} 를 미제출하였습니다."
+        elif len(student['daily_done']) <= 3:
+            daily_str = f"일일학습지는 {' / '.join(student['daily_done'])} 를 제출하였고, {' / '.join(student['daily_not_done'])} 를 미제출하였습니다."
+        else:
+            daily_str = f"일일학습지는 {' / '.join(student['daily_done'])} 를 제출하였습니다."
+        return daily_str
+
+    # 테스트 점수 문자
+    def mk_test_str(self, student):
+        if student['점수'] == '미응시':
+            test_str = '미응시'
+        else:
+            test_str = str(int(round(student['점수'])))+'점'
+        return test_str
+            
+    # 문자 생성
+    def make_text(self, student, date, mean_score, next_hw_str):
+        daily_str = self.mk_daily_str(student)
+        test_str = self.mk_test_str(student)
+        text = f'''<대치에스학원 박래혁수학>
+
+{student['이름']} 학생의 {date[0][0]}월 {date[0][1]}일 ~ {date[1][0]}월 {date[1][1]}일 숙제 제출 및 테스트 결과입니다.
+
+주간테스트 점수는 {test_str} / 평균은 {mean_score}점 입니다.
+숙제 제출 상태는 {student['숙제 제출 여부']} 입니다.
+{daily_str}
+
+다음주 숙제는 {next_hw_str} 입니다.
+
+감사합니다.
+
+(A : 완벽 / B : 미흡 / C : 불량 / D : 미제출 )'''
+        return text
+
+
+    def save_text_files(self, folder_path, df_students):
+        # 폴더 경로 생성
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        else:
+            for file in os.scandir(folder_path):
+                os.remove(file.path)
+
+        # 데이터프레임 루프
+        for name, text_content in zip(df_students['이름'],df_students['text']):
+            # 파일 이름 생성
+            filename = f"{folder_path}/{name}.txt"
+            
+            # 파일 생성 및 값 작성
+            with open(filename, "w") as f:
+                f.write(text_content)
